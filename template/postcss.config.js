bb346ea4ec775fac9854c7a9d5dd723f140464f8
zhaomenghuan <1028317108@qq.com>
2017-05-16 11:39:05 +0800

    vue-plugin-template init

---
+++ template/postcss.config.js
@@ -0,0 +1,9 @@
+const cssnext = require('postcss-cssnext');
+const autoprefixer = require('autoprefixer');
+module.exports = {
+  // parser: 'sugarss',
+  // plugins: {
+  //   'cssnext': {},
+  //   'autoprefixer': {}
+  // }
+}
---
+++ template/webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: (process.env.NODE_ENV === 'production') ? './' : '/',
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|vue)$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: /src/,
+                options: {
+                    formatter: require('eslint-friendly-formatter')
+                }
+            },
+            {
+                test: /\.vue$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                include: /src/
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'img/[name]_[hash:7].[ext]'
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'fonts/[name].[hash:7].[ext]'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: (process.env.NODE_ENV === 'production') ? '#source-map' : false,
+};
+
+if (process.env.NODE_ENV === 'development') {
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': 'development'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ])
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: 'production'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
